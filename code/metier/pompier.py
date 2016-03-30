d8010a5d62b7bbe024a470539181fee3927595e5
azarz <amaury.zarzelli@ensg.eu>
2016-03-30 16:50:59 +0200

    la màj des pompiers

--- code/metier/pompier.py
+++ code/metier/pompier.py
@@ -19,22 +19,69 @@ class Pompier(object):
         self.x=x
         self.y=y
         
+        
     def deplacement(self,dx,dy):
         self.x += dx
         self.y += dy
+        
         
     def cherche_feu(self,liste_brule):
         x=self.x
         y=self.y
         dist=float('inf')
-        cellule=liste_brule[0]
+        case=liste_brule[0]
         
         for cell in liste_brule:
             temp = distance(x,y, cell.x,cell.y)
             if temp < dist:
                 dist = temp
-                cellule=cell
+                case=cell
         
-        return cellule
+        return case
             
+            
+    def aller_vers_feu(self,case_feu,carte):
+        
+        case=carte.cherche(self.x,self.y)
+        
+        if case.etat > 0:
+            adj=case.adjacence()
+            echappatoire=adj[0]
+            
+            for cell in adj:
+                if cell.etat < echappatoire.etat:
+                    echappatoire=cell
+            
+            self.x=echappatoire.x
+            self.y=echappatoire.y
+            
+        else:
+            if self.x < case_feu.x and self.y < case_feu.y:
+                self.deplacement(1,1)
+            elif self.x > case_feu.x and self.y > case_feu.y:
+                self.deplacement(-1,-1)
+            elif self.x < case_feu.x and self.y > case_feu.y:
+                self.deplacement(1,-1)
+            elif self.x > case_feu.x and self.y < case_feu.y:
+                self.deplacement(-1,1)
+            elif self.x > case_feu.x and self.y == case_feu.y:
+                self.deplacement(-1,0)
+            elif self.x == case_feu.x and self.y < case_feu.y:
+                self.deplacement(0,1)
+            elif self.x == case_feu.x and self.y > case_feu.y:
+                self.deplacement(0,-1)
+            elif self.x > case_feu.x and self.y == case_feu.y:
+                self.deplacement(1,0)
+                
+            
+        nouv_case=carte.cherche(self.x,self.y)
+        adj=nouv_case.adjacence()
+        
+        return nouv_case, adj
+    
+    
+
+    def eteindre_feu(self, case, liste_adj, liste_brule, carte):
+        pass
+        
         
