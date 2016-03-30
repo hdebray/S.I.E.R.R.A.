dd2efe96cbdea0837b9ab1e6fd11a27b82c03031
azarz <amaury.zarzelli@ensg.eu>
2016-03-30 15:39:47 +0200

    chercher_feu des pompiers

--- code/metier/pompier.py
+++ code/metier/pompier.py
@@ -4,6 +4,12 @@ Created on Wed Mar 30 15:20:49 2016
 
 @author: Amaury
 """
+import numpy as np
+
+
+def distance(x1,y1,x2,y2):
+    return np.sqrt((x1-x2)**2 + (y1-y2)**2)
+
 
 class Pompier(object):
     
@@ -17,5 +23,18 @@ class Pompier(object):
         self.x += dx
         self.y += dy
         
-    
-    
+    def cherche_feu(self,liste_brule):
+        x=self.x
+        y=self.y
+        dist=float('inf')
+        cellule=liste_brule[0]
+        
+        for cell in liste_brule:
+            temp = distance(x,y, cell.x,cell.y)
+            if temp < dist:
+                dist = temp
+                cellule=cell
+        
+        return cellule
+            
+        
