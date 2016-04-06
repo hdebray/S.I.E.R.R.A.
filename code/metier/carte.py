f3a1c756a8aabff32c256c40c94011e375419f1b
azarz <amaury.zarzelli@ensg.eu>
2016-04-06 15:54:20 +0200

    anglicisation, 2ème

--- code/metier/carte.py
+++ code/metier/carte.py
@@ -8,80 +8,80 @@ Created on Mon Mar 28 11:49:48 2016
 import numpy as np
 import random as rnd
 
-import metier.pompier as pom
-import metier.case as cs
-import affichage.affiche as af
-import bdd.donnee as bd
+import metier.fireman as frm
+import metier.cell as cl
+import affichage.display as display
+import bdd.data as db
 
     
 
 
 """
-Création d'une carte en combinant deux cartes de hauteur (heightmap), 
-générées par un bruit de valeur (Value Noise)
+Création d'une map en combinant deux maps de hauteur (heightmap), 
+générées par un noise de valeur (Value Noise)
 """
-class Carte(object):
-    def __init__(self,taille):
-        self.taille = taille
-        self.carte = np.zeros([taille,taille])
-        self.liste_case = []        #liste des cases qui composent la carte
-        self.liste_brule = []       #liste des cases en train de bruler à chaque itération
-        self.liste_pompier = []     #liste des pompiers actifs sur la carte
-        self.iter = 0               #numéro de l'itération au cours de la simulation
+class Map(object):
+    def __init__(self,size):
+        self.size = size
+        self.map = np.zeros([size,size])
+        self.cell_list = []        #liste des cells qui composent la map
+        self.burn_list = []       #liste des cells en train de bruler à chaque itération
+        self.fireman_list = []     #liste des firemans actifs sur la map
+        self.count = 0               #numéro de l'itération au cours de la simulation
         
         
     def creation(self):
-        """Combine deux heightmap (altitude et humidité), puis construit la carte qui en dépend"""
+        """Combine deux heightmap (altitude et humidité), puis construit la map qui en dépend"""
         alti = self.heightmap()
-        foret = self.heightmap()
-        for i in range(self.taille):
-            for j in range(self.taille):
-                self.carte[i,j] = self.biome(alti,foret,i,j)        #créé la carte
+        forest = self.heightmap()
+        for i in range(self.size):
+            for j in range(self.size):
+                self.map[i,j] = self.biome(alti,forest,i,j)        #créé la map
         
                 
-    def biome(self,alti,foret,i,j):
-        """Construit la carte en combinant les valeurs des deux heightmap"""
+    def biome(self,alti,forest,i,j):
+        """Construit la map en combinant les valeurs des deux heightmap"""
         n = 0
         if(alti[i,j] < 0.3):        #eau
             n = 0
-            self.liste_case.append(cs.Case(j,i,0))
-        elif(alti[i,j] > 0.6 and foret[i,j] > 0.5):     #foret
+            self.cell_list.append(cl.Cell(j,i,0))
+        elif(alti[i,j] > 0.6 and forest[i,j] > 0.5):     #forest
             n = 2
-            self.liste_case.append(cs.Case(j,i,2))
-        elif(alti[i,j] > 0.7 and foret[i,j] < 0.4):     #maison
+            self.cell_list.append(cl.Cell(j,i,2))
+        elif(alti[i,j] > 0.7 and forest[i,j] < 0.4):     #maison
             n = 3
-            self.liste_case.append(cs.Case(j,i,3))
+            self.cell_list.append(cl.Cell(j,i,3))
         else:               #plaine
             n = 1
-            self.liste_case.append(cs.Case(j,i,1))
+            self.cell_list.append(cl.Cell(j,i,1))
         return n
         
     def heightmap(self,amp=1,freq=1/2.0):
-        """Construit une heightmap en ajoutant successivement des bruits de valeurs,
+        """Construit une heightmap en ajoutant successivement des noises de valeurs,
         à des amplitudes et des fréquences différentes"""
-        bruit = np.zeros([self.taille,self.taille])     #initialisation
-        while(self.taille*freq > 1 and amp > 0.01):     #tant que la résolution de la carte est supérieur à 1
-            bruit += self.calc_bruit(self.taille*freq) * amp
+        noise = np.zeros([self.size,self.size])     #initialisation
+        while(self.size*freq > 1 and amp > 0.01):     #tant que la résolution de la map est supérieur à 1
+            noise += self.calc_noise(self.size*freq) * amp
             freq *= 0.4     #augmentation de la fréquence
             amp *= 0.6    #diminution de l'amplitude
-        bruit = self.distrib(bruit)
-        return bruit
+        noise = self.distrib(noise)
+        return noise
         
-    def calc_bruit(self,res):
-        """Calcul un bruit de valeur pour une résolution données (taille*fréquence).
-        La graine utilisé pour générer le bruit blanc de départ est différente à chaque fois"""
+    def calc_noise(self,res):
+        """Calcul un noise de valeur pour une résolution données (size*fréquence).
+        La graine utilisé pour générer le noise white de départ est différente à chaque fois"""
         perm = np.random.permutation(256)           #table de permutation aléatoire (graine du monde)
-        self.blanc = self.bruit_blanc(self.taille,perm)                #bruit blanc
+        self.white = self.white_noise(self.size,perm)                #noise white
         
-        temp = np.zeros([self.taille,self.taille])
-        for i in range(self.taille):
-            for j in range(self.taille):
+        temp = np.zeros([self.size,self.size])
+        for i in range(self.size):
+            for j in range(self.size):
                 m,n = int(i/res),int(j/res)
-                temp[i][j] = self.lissage(i/res,j/res)      #interpolation lissée du bruit blanc
+                temp[i][j] = self.smooth(i/res,j/res)      #interpolation lissée du noise white
                 
         return temp
         
-    def lissage(self,y,x):
+    def smooth(self,y,x):
         """Cette fonction lisse l'interpolation pour avoir un meilleur résultat, en utilisant
         une interpolation bilinéaire"""
         x0 = int(x)         #partie entière
@@ -91,21 +91,21 @@ class Carte(object):
         
         x1 = (x0+1)
         y1 = (y0+1)
-        if(x1 > len(self.blanc)-1): x1 = len(self.blanc)-1
-        if(y1 > len(self.blanc)-1): y1 = len(self.blanc)-1
+        if(x1 > len(self.white)-1): x1 = len(self.white)-1
+        if(y1 > len(self.white)-1): y1 = len(self.white)-1
         
-        dx1 = self.lerp(frac_x,self.blanc[y0][x0],self.blanc[y0][x1])        #interpolation bilinéaire de la valeur du pixel
-        dx2 = self.lerp(frac_x,self.blanc[y1][x0],self.blanc[y1][x1])
+        dx1 = self.lerp(frac_x,self.white[y0][x0],self.white[y0][x1])        #interpolation bilinéaire de la valeur du pixel
+        dx2 = self.lerp(frac_x,self.white[y1][x0],self.white[y1][x1])
         result = self.lerp(frac_y,dx1,dx2)
         return result
         
     def lerp(self,p,a,b):
         """Interpolation linéaire de la valeur en p, selon la valeur au point a et au point b"""
-        t = (p**2)*(3 - 2*p)          #lissage de l'interpolation, cubique 
+        t = (p**2)*(3 - 2*p)          #smooth de l'interpolation, cubique 
         return a*(1-t) + b*t        #interpolation
         
-    def bruit_blanc(self,dim,perm):
-        """Génération d'un bruit blanc, selon une graine (table de permutation)"""
+    def white_noise(self,dim,perm):
+        """Génération d'un noise white, selon une graine (table de permutation)"""
         mat = np.zeros([dim,dim])
         for i in range(dim):
             for j in range(dim):
@@ -122,101 +122,101 @@ class Carte(object):
     """
     Modification et mise à jour de l'état de la simulation
     """
-    def ini(self,foyer=-1,pompiers=-1):
-        if(foyer < 0): foyer = int( np.ceil(self.taille/30) )
+    def ini(self,foyer=-1,firemans=-1):
+        if(foyer < 0): foyer = int( np.ceil(self.size/30) )
         self.johny(foyer)               #allume le feu
-        if(pompiers < 0): pompiers = int( np.ceil(self.taille/3) )
-        self.creer_pompier(pompiers)     #créer les pompiers
+        if(firemans < 0): firemans = int( np.ceil(self.size/3) )
+        self.create_fireman(firemans)     #créer les firemans
         
-    def tour(self):
-        if(self.iter == 0 and len(self.liste_brule) < 1): self.ini()     #initialisation si cela n'a pas été fait
-        self.iter+= 1
-        k = len(self.liste_brule)
+    def turn(self):
+        if(self.count == 0 and len(self.burn_list) < 1): self.ini()     #initialisation si cela n'a pas été fait
+        self.count+= 1
+        k = len(self.burn_list)
         i = 0
         while(i < k):
-            if(len(self.liste_brule) > 0):
-                cell = self.cherche(self.liste_brule[i].x,self.liste_brule[i].y)        #on récupère la cellule qui va être propagé 
+            if(len(self.burn_list) > 0):
+                cell = self.search(self.burn_list[i].x,self.burn_list[i].y)        #on récupère la cellule qui va être propagé 
                 cell.propagation(self)
-                if(cell.carbo == True): k -= 1      #si la case vient d'être carbonisé, la taille de liste_brule a diminué
+                if(cell.charred == True): k -= 1      #si la cell vient d'être charrednisé, la size de burn_list a diminué
                 i += 1
                 
-        af.dessine(self,nom='a')
+        display.draw(self,name='a')
         
-        for pmp in self.liste_pompier:
-            pmp.agir(self)
-            if(pmp.pv <= 0): self.liste_pompier.remove(pmp)         #le pompier est mort
+        for frm in self.fireman_list:
+            frm.agir(self)
+            if(frm.pv <= 0): self.fireman_list.remove(frm)         #le fireman est mort
             
-        af.dessine(self,nom='b')
+        display.draw(self,name='b')
             
     
     def johny(self,n):
         """ ALLUMMEEEEEEEEEEEEEEEERR,  LE FEEUUU !! """
-        for i in range(n):      #n: nombre de foyer de feu au début de la simulation
+        for i in range(n):      #n: namebre de foyer de feu au début de la simulation
             b = True
             while(b):           #boucle de test pour ne pas bruler l'eau
                 b = False
-                x,y = rnd.randint(0,self.taille-1),rnd.randint(0,self.taille-1)
-                case = self.cherche(x,y)
-                if(case.nat == 0): b = True
-            case.etat = 1
-            self.liste_brule.append(case)
+                x,y = rnd.randint(0,self.size-1),rnd.randint(0,self.size-1)
+                cell = self.search(x,y)
+                if(cell.nat == 0): b = True
+            cell.state = 1
+            self.burn_list.append(cell)
             
             
-    def creer_pompier(self,n):
-        for i in range(n):      #n: nombre de pompiers au début de la simulation
-            nom = "august"+str(i)
+    def create_fireman(self,n):
+        for i in range(n):      #n: namebre de firemans au début de la simulation
+            name = "august"+str(i)
             b = True
-            while(b):           #boucle de test pour ne pas mettre un pompier dans l'eau
+            while(b):           #boucle de test pour ne pas mettre un fireman dans l'eau
                 b = False
-                x,y = rnd.randint(0,self.taille-1),rnd.randint(0,self.taille-1)
-                case = self.cherche(x,y)
-                if(case.nat == 0 or case.etat > 0): b = True
-            self.liste_pompier.append(pom.Pompier(nom,x,y))     #créer un nouveau pompier et l'ajoute à la liste
+                x,y = rnd.randint(0,self.size-1),rnd.randint(0,self.size-1)
+                cell = self.search(x,y)
+                if(cell.nat == 0 or cell.state > 0): b = True
+            self.fireman_list.append(frm.Fireman(name,x,y))     #créer un nouveau fireman et l'ajoute à la liste
         
         
-    def cherche(self,x,y):
-        """Cette fonction cherche une case aux coordonnées x et y dans une liste d'objets Case"""
+    def search(self,x,y):
+        """Cette fonction search une cell aux coordonnées x et y dans une liste d'objets Cell"""
         result = None
-        for case in self.liste_case:
-            if(case.x == x and case.y == y): result = case
+        for cell in self.cell_list:
+            if(cell.x == x and cell.y == y): result = cell
         return result
         
-    def calcul_mat(self):
-        """Cette fonction recalcule la matrice self.carte en fonction des cases dans la liste_case.
-        Elle n'est normalement utilisé que pour l'affichage"""
-        self.carte = np.zeros([self.taille,self.taille])
-        for cell in self.liste_brule:       #on affecte d'abord les cases en feu
+    def calc_mat(self):
+        """Cette fonction recalce la matrice self.map en fonction des cells dans la cell_list.
+        Elle n'est normalement utilisé que pour l'displayfichage"""
+        self.map = np.zeros([self.size,self.size])
+        for cell in self.burn_list:       #on displayfecte d'abord les cells en feu
             i,j = cell.y,cell.x
-            self.carte[i,j] = 3 + cell.etat
+            self.map[i,j] = 3 + cell.state
         
-        for cell in self.liste_case:
+        for cell in self.cell_list:
             i,j = cell.y,cell.x
-            if(self.carte[i,j] == 0):       #si une valeur n'a pas déja été affecté
-                    if(cell.carbo == True):     #si c'est une case carbo, on lui affecte le maximum +1 (pour l'affichage)
-                        self.carte[i,j] = -1
+            if(self.map[i,j] == 0):       #si une valeur n'a pas déja été displayfecté
+                    if(cell.charred == True):     #si c'est une cell charred, on lui displayfecte le maximum +1 (pour l'displayfichage)
+                        self.map[i,j] = -1
                     else:
-                        self.carte[i,j] = cell.nat      #sinon, on affecte le chiffre correspondant à la nature
+                        self.map[i,j] = cell.nat      #sinon, on displayfecte le chiffre correspondant à la nature
         
         
-    def sauvegarde(self):
+    def save(self):
         """Sauvegarde l'état actuel de la matrice"""
-        bd.sauve_carte(self.liste_case,self.iter)
-        bd.sauve_pompier(self.liste_pompier,self.iter)
+        db.save_map(self.cell_list,self.count)
+        db.save_fireman(self.fireman_list,self.count)
         
-    def construit(self,num):
-        self.liste_case = []
-        self.liste_brule = []
+    def construct(self,num):
+        self.cell_list = []
+        self.burn_list = []
         
-        result = bd.recup_case(num)
-        for ligne in result:
-            case = cs.Case(ligne[0],ligne[1],ligne[2],ligne[3],ligne[4])
-            self.liste_brule.append(case)
-            if(ligne[3] > 0 and ligne[4] == False):     #intensité > 0 et carbo = Fasle
-                self.liste_brule.append(case)
+        result = db.get_cell(num)
+        for line in result:
+            cell = cl.Cell(line[0],line[1],line[2],line[3],line[4])
+            self.burn_list.append(cell)
+            if(line[3] > 0 and line[4] == False):     #intensité > 0 et charred = Fasle
+                self.burn_list.append(cell)
                 
-        self.liste_pompier = []
+        self.fireman_list = []
         
-        result = bd.recup_pompier(num)
-        for ligne in result:
-            pomp = pom.Pompier(ligne[0],ligne[1],ligne[2],ligne[3])
-            self.liste_pompier.append(pomp)
+        result = db.recup_fireman(num)
+        for line in result:
+            frmp = frm.Fireman(line[0],line[1],line[2],line[3])
+            self.fireman_list.append(frmp)
