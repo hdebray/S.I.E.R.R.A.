7acd59af3c5a6b74bcd94bc863ae734b94ebd7cb
Ogus <ogustin.gagnon@gmail.com>
2016-04-08 12:21:37 +0200

    Add notification when fireman dies

--- code/gui/display.py
+++ code/gui/display.py
@@ -53,61 +53,15 @@ Create a discrete colormap, with hexa color codes
 """
 cpool = ['#2a0d03','#318fe5', '#51c353', '#4e8539', '#b6aba2', 
          '#dc6741', '#d44212', '#a9340e', '#7f270a', '#541a07']
-         
 
 
-#"""Sauvegarde des colors de simulation, une par intensité maximale sur la map"""
-#sim_0 = col.ListedColormap(cpool[1:5], 'indexed')       #pas de feu
-#cm.register_cmap(cmap=sim_0)
-#sim_1 = col.ListedColormap(cpool[1:6], 'indexed')       #intensité max = 1
-#cm.register_cmap(cmap=sim_1)
-#sim_2 = col.ListedColormap(cpool[1:7], 'indexed')       #intensité max = 2
-#cm.register_cmap(cmap=sim_2)
-#sim_3 = col.ListedColormap(cpool[1:8], 'indexed')       #intensité max = 3
-#cm.register_cmap(cmap=sim_3)
-#sim_4 = col.ListedColormap(cpool[1:9], 'indexed')       #intensité max = 4
-#cm.register_cmap(cmap=sim_4)
-#sim_5 = col.ListedColormap(cpool[1:10], 'indexed')       #intensité max = 5
-#cm.register_cmap(cmap=sim_5)
-#
-#"""Couleurs utilisés si il y a des cases charrednisées"""
-#sim_0_c = col.ListedColormap(cpool[0:5], 'indexed')
-#cm.register_cmap(cmap=sim_0_c)
-#sim_1_c = col.ListedColormap(cpool[0:6], 'indexed')
-#cm.register_cmap(cmap=sim_1_c)
-#sim_2_c = col.ListedColormap(cpool[0:7], 'indexed')
-#cm.register_cmap(cmap=sim_2_c)
-#sim_3_c = col.ListedColormap(cpool[0:8], 'indexed')
-#cm.register_cmap(cmap=sim_3_c)
-#sim_4_c = col.ListedColormap(cpool[0:9], 'indexed')
-#cm.register_cmap(cmap=sim_4_c)
-#sim_5_c = col.ListedColormap(cpool[0:10], 'indexed')
-#cm.register_cmap(cmap=sim_5_c)
-
-
-def draw(map,svg=True,name='',hide=True,colorbar=False):
+def draw(map,svg=True,name='',hide=True,colorbar=False,text=[]):
     """Display the map with the matrix of values.
     svg to save the image, name for specific name, hide to hide the image, colorbar to show the colorbar
     """
     map.calc_mat()      #update the matrix
     mx = int( np.amax(map.map) )      #max and min values of the matrix
     mn = int( np.amin(map.map) )      #to determine the colormap used
-        
-        
-#    if(mn == 0):            #on détermine les colors utilisés pour afficher le terrain
-#        if(mx <= 3.0): color = sim_0
-#        elif(mx == 4.0): color = sim_1
-#        elif(mx == 5.0): color = sim_2
-#        elif(mx == 6.0): color = sim_3
-#        elif(mx == 7.0): color = sim_4
-#        elif(mx == 8.0): color = sim_5
-#    else:                   #on utilise les colors spécifiques au cas "charrednisé"
-#        if(mx <= 3.0): color = sim_0_c
-#        elif(mx == 4.0): color = sim_1_c
-#        elif(mx == 5.0): color = sim_2_c
-#        elif(mx == 6.0): color = sim_3_c
-#        elif(mx == 7.0): color = sim_4_c
-#        elif(mx == 8.0): color = sim_5_c
     
     
     simu_color = col.ListedColormap(cpool[(mn+1):(mx+2)], 'indexed')
@@ -116,13 +70,33 @@ def draw(map,svg=True,name='',hide=True,colorbar=False):
         
     if(hide): plt.ioff()         #hide the poping windows of python
     else: plt.ion()
-        
-    plt.matshow(map.map,cmap=color)
     
     if(colorbar): plt.colorbar()     #show the colormap used to draw the matrix
+    
+    
+    #list of displayed fireman on the map, to draw bigger symbols when there is multiple firemen on same cell
+    frman_display = []
+    for frman in map.fireman_list:
+        new = True
+        for i in range(len(frman_display)):
+            if(frman.x == frman_display[i][0] and frman.y == frman_display[i][1]):       #if there is already a fireman on this position
+                new = False
+                break
+        if(not new): frman_display[i][2] += 1       #size of the symbol whill be bigger
+        else: frman_display.append([frman.x,frman.y,0])     #new position to draw a symbol
+                
+                
+    
+    plt.matshow(map.map,cmap=color)     #display the map
+    
+    
+    for i in range(len(frman_display)):          #display firemen with a red square
+        size = 3 + (frman_display[i][2] * 2)
+        plt.plot(frman_display[i][0],frman_display[i][1],'rs',markersize=size)
+    
+    for i in range(len(text)):
+        plt.text(0,i, text[i], color='w')
         
-    for pompier in map.fireman_list:     #display firemen with a red square, 3 pixel wide
-        plt.plot(pompier.x,pompier.y,'rs',markersize=3)
         
     plt.axis([-0.5,map.size-0.5,-0.5,map.size-0.5])     #resize the image
     plt.axis('off')                                     #hide the axis
